6f685baff0e83a53429581ed2afdc7437e216491
Wendy <754610995@qq.com>
2020-08-26 16:35:49 +0800

    使用 amfe-flexible+px2rem-loader 进行移动端 H5适配

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 手淘 H5 适配
+import 'amfe-flexible/index.js'
 
 Vue.config.productionTip = false
 
