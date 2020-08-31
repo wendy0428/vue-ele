09f7eae6786a8f8135aae94cbb2ea3fab1cea53c
Wendy <754610995@qq.com>
2020-08-31 15:11:39 +0800

    安装 axios

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 // 手淘 H5 适配
 import 'amfe-flexible/index.js'
 
 Vue.config.productionTip = false
+Vue.prototype.axios = axios
 
 new Vue({
   router,
