af0a64128a2ee770ac2a834cc62fcd29b3086cdc
Wendy <754610995@qq.com>
2020-09-02 17:54:36 +0800

    首页页面

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,10 +1,24 @@
 <template>
-    <div id="home">1111</div>
+    <div id="home">
+        <common-head></common-head>
+    </div>
 </template>
 <script>
+// 公共头部组件
+const commonHead = () => import('@/components/header/head')
+import {cityGuess} from '../../service/getDate'
 export default {
     data(){
         return {}
+    },
+    components:{
+        commonHead
+    },
+    created(){
+        let city = cityGuess().then((res)=>{
+            console.log('res',res);
+        })
+     
     }
 }
 </script>
