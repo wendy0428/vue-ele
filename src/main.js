4d722de9736681de62cbf24a0eac5f41bb6ef0a2
Wendy <754610995@qq.com>
2020-09-03 14:15:31 +0800

    axios 封装

--- src/main.js
+++ src/main.js
@@ -9,11 +9,6 @@ import 'amfe-flexible/index.js'
 Vue.config.productionTip = false
 
 Vue.prototype.axios = axios
-// axios 全局配置
-axios.defaults.timeout = 15000;  //超时响应
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'; // 配置请求头（推荐）
-// axios.defaults.baseURL = $core.use('http'); //确认协议和地址
-axios.defaults.withCredentials = true;   // axios 默认不发送cookie，需要全局设置true发送cooki
 
 new Vue({
   router,
--- src/service/getHttp.js
+++ src/service/getHttp.js
@@ -1,7 +1,52 @@
-import {
-	baseUrl
-} from './env'
+import { baseUrl } from './env'
+import { getStore,setStore } from './mUtils'
+import store from '../store'
 import axios from 'axios' 
+// request 拦截器实现
+axios.interceptors.request.use(
+	config => {
+		config.baseURL = baseUrl;
+	 	config.withCredentials = true ;// 允许携带token ,这个是解决跨域产生的相关问题
+		config.timeout = 6000;
+	 	let token = getStore('access_token')
+	 	if (token) {
+			config.headers = {
+	   			'access-token': token,
+	   			'Content-Type': 'application/x-www-form-urlencoded'
+	  		}
+	 	}
+	 	if (config.url === 'refresh') {
+	  		config.headers = {
+	   			'refresh-token': getStore('refresh_token'),
+	   			'Content-Type': 'application/x-www-form-urlencoded'
+	  		}
+	 	}
+	 	return config
+	},
+	error => {
+	 	return Promise.reject(error)
+	}
+)
+
+// response 拦截器实现
+axios.interceptors.response.use(
+	response => {
+	 	// 定时刷新access-token
+	 	if (!response.data && response.data.data.message === 'token invalid') {
+	  		// 刷新token
+	  		store.dispatch('refresh').then(response => {
+				setStore('access_token', response.data)
+	  		}).catch(error => {
+	   			throw new Error('token刷新' + error)
+	  		})
+	 	}
+	 	return response
+	},
+	error => {
+	 	return Promise.reject(error)
+	}
+)
+
 
 // get 请求
 export function httpGet({
@@ -11,7 +56,7 @@ export function httpGet({
 }){
 	return new Promise((resolve, reject) => {
 		axios({
-			url: baseUrl+url,
+			url,
 			type,
 			params,
 		}).then((res) => {
@@ -29,8 +74,9 @@ export function httpPost({
   }) {
 	return new Promise((resolve, reject) => {
 		axios({
-			url: baseUrl+url,
+			url,
 			method: 'POST',
+			// post请求提交前需要对它进行序列号操作，这里是通过transformRequest做处理
 			transformRequest: [function (data) {
 				let ret = ''
 			  	for (let it in data) {
