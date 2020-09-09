ebe85706d76a06413d8c746b1c553d874dc9099c
Wendy <754610995@qq.com>
2020-09-09 13:23:40 +0800

    商铺列表组件

---
+++ src/components/common/shopList.vue
@@ -0,0 +1,54 @@
+<template>
+    <div id="CommonshopList">
+        <ul v-if="list.length!=0">
+            <router-link :to="{}" tag="li"
+            v-for="(shop,shopIndex) in list" :key="shopIndex"
+            >
+                <div class="box_left">
+                    <img :src="'http://cangdu.org:8001/img/'+shop.image_path"/>
+                </div>
+                <div class="box_right">
+                    <!-- 第一行 -->
+                    <div class="box_right_top">
+                        <div class="box_right_top_name">
+                            <span>品牌</span>
+                            <span>shop.name</span>
+                        </div>
+                        <div class="box_right_top_icon_name" v-if="shop.supports&&shop.supports.length!=0">
+                            <li v-for="(icon,iconIndex) in shop.supports" :key="iconIndex">
+                                {{icon.icon_name}}
+                            </li>
+                        </div>
+                    </div>
+                    <!-- 第二行 -->
+                    <div class="box_right_mid">
+                        <rating-star :rating="rating"></rating-star>
+                    </div>
+                </div>
+            </router-link>
+        </ul>
+    </div>
+</template>
+<script>
+// 公共域名
+import {imgBaseUrl} from '../../config/env'
+// 引入评分组件
+import ratingStar from './ratingStar'
+export default {
+    data(){
+        return {}
+    },
+    components:{
+        ratingStar
+    },
+    props:{
+        list: Array
+    },
+    methods:{
+
+    }
+}
+</script>
+<style scoped>
+
+</style>
