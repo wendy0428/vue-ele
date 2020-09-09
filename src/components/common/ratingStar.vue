54929a32be7a25ab239e753ae64b247e96ff1386
Wendy <754610995@qq.com>
2020-09-09 13:23:16 +0800

    评分组件

---
+++ src/components/common/ratingStar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="ratingStar">
+
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    },
+    props:{
+        rating
+    }
+}
+</script>
+<style scoped>
+
+</style>
