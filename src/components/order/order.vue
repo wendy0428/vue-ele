3bd7e7f4979e6600740ab0ee9a981f4667811575
Wendy <754610995@qq.com>
2020-09-25 13:37:34 +0800

    订单页面

--- src/components/order/order.vue
+++ src/components/order/order.vue
@@ -6,7 +6,30 @@
                <span></span>
             </template>
         </common-head>
-        <section class="count_info">
+        <section class="order_list" v-if="orderList.length!=0">
+            <ul>
+                <li v-for="(order,orderIndex) in orderList" :key="orderIndex">
+                    <div class="order_left">
+                        <img :src="'http://cangdu.org:8001/img/'+order.restaurant_image_url"/>
+                    </div>
+                    <div class="order_right">
+                        <div class="order_right_top">
+                            <span>{{order.restaurant_name}}</span>
+                            <span>支付超时</span>
+                        </div>
+                        <div class="order_right_middle">
+                            <span>{{order.formatted_created_at}}</span>
+                        </div>
+                        <div class="order_right_bottom">
+                            <span class="total_quantity">{{order.basket.group[0][0].name}}{{order.basket.group[0].length>1?'等'+order.total_quantity+'件商品':''}}</span>
+                            <span class="total_amount">¥{{order.total_amount}}</span>
+                        </div>
+                        <!-- <div class="oneMore"> -->
+                            <router-link :to="{path:'/shop',query:{geograph,id:order.restaurant_id}}" class="oneMore" tag="span">再来一单</router-link>
+                        <!-- </div> -->
+                    </div>
+                </li>
+            </ul>
         </section>
         <!-- 公共的底部 Tabber 组件 -->
         <tabbar :selectedTab="selectedTab"></tabbar>
@@ -28,9 +51,11 @@ export default {
             orderList: [], // 订单列表
             offset: 10,
             selectedTab:'订单',
+            geograph:'',
         }
     },
     created(){
+        this.geograph = getStore('latitude') + ',' + getStore('longitude') ;
         this.initData();
     },
     methods:{
@@ -56,5 +81,65 @@ export default {
     background-color: #f2f2f2;
     z-index: 11;
 }
+.order_list{
+    margin-top: 90px;
+}
+.order_list ul{
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+.order_list ul li{
+    display: flex;
+    /* font-size: 30px; */
+    background: #fff;
+}
+.order_left{
+    width: 25%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+.order_left img{
+    width: 80%;
+}
+.order_right{
+    width: 75%;
+    text-align: right;
 
+}
+.order_right>div{
+    padding: 5px 20px;
+}
+.order_right_top{
+    color:  #333;
+    font-size: 35px;
+    display: flex;
+    justify-content: space-between;
+}
+.order_right_middle{
+    font-size: 30px;
+    color: #999;
+    text-align: left;
+}
+.order_right_bottom{
+    display: flex;
+    justify-content: space-between;
+}
+.order_right_bottom{
+    font-size: 35px;
+    color: #666;
+}
+.total_amount{
+    color: #f60;
+    font-weight: bold;
+}
+
+.oneMore{
+    font-size: 30px;
+    color: #3190e8;
+    border: 1px solid #3190e8;   
+    margin-right: 20px;
+    border-radius: 10px;
+}
 </style>
